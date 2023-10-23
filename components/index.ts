e3da4641b5b25dd17dd0ac413821981d9cd28942
Nguyen Hoang Phuong Nam <nhpn2003@gmail.com>
2023-10-23 13:51:30 +0700

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CardDetails from "./CardDetails";
+import ShowMore from "./ShowMore";
+export {
+    Hero,
+    CustomButton,
+    Nav,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    CardDetails,
+    ShowMore
+}
